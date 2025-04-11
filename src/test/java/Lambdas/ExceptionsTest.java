25fa7266e6bd17f92540b6fde6b92ab32a35e205
Maria Molina <maria.a1qa@innocaption.com>
2025-04-10 22:04:21 -0600

    Exception examples

--- src/test/java/Lambdas/ExceptionsTest.java
+++ src/test/java/Lambdas/ExceptionsTest.java
@@ -1,4 +1,51 @@
 package Lambdas;
 
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ExceptionsTest {
+
+    @Test
+    public void tryCatchTest() {
+        try {
+            int x = 10;
+            int y = 0;
+            int result = x / y;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+
+    @Test
+    public void throwException() throws MyException {
+        checkAge(10);
+    }
+
+    @Test
+    public void unCheckedException() {
+        divide(10, 0);
+    }
+
+    @Test
+    public void checkedException() throws IOException {
+        readFile("some name");
+    }
+
+    void checkAge(int age) throws MyException {
+        if (age < 18) {
+            throw new MyException("You must be 18 or older");
+        }
+    }
+
+    public void divide(int a, int b) {
+        int result = a / b;
+    }
+
+    public void readFile(String filename) throws IOException {
+        FileReader file = new FileReader(filename);
+        file.read();
+        file.close();
+    }
 }
---
+++ src/test/java/Lambdas/MyException.java
@@ -0,0 +1,7 @@
+package Lambdas;
+
+public class MyException extends Exception {
+    MyException(String message) {
+        super(message);
+    }
+}
